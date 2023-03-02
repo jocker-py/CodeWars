ae691c1ddacc7b4715c5cced520b9d21dfc49491
jocker-py <buladzislau@gmail.com>
2023-03-02 11:31:25 +0300

    feat: add solution for 'Grasshopper - Terminal Game #1'

---
+++ 8kyu/Grasshopper - Terminal Game #1.js
@@ -0,0 +1,25 @@
+/*
+Terminal Game - Create Hero Prototype
+In this first kata in the series,
+you need to define a Hero prototype
+to be used in a terminal game.
+
+The hero should have the following attributes:
+attribute	  value
+name	      user argument or 'Hero'
+position	  '00'
+health	    100
+damage	    5
+experience	0
+ */
+
+function Hero (name) {
+  // add default values here
+  return {
+    name: name ? name : 'Hero',
+    position: '00',
+    health: 100,
+    damage: 5,
+    experience: 0
+  }
+}

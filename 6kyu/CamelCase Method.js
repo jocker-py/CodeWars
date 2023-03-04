f2a176a2a4e7823cc9ee45eeb713c8248423c55b
jocker-py <buladzislau@gmail.com>
2023-03-04 12:40:16 +0300

    feat: add solution for task 'CamelCase Method'

---
+++ 6kyu/CamelCase Method.js
@@ -0,0 +1,22 @@
+/*
+link: https://www.codewars.com/kata/587731fda577b3d1b0001196/train/javascript
+
+Write simple .camelCase method (camel_case function in PHP,
+CamelCase in C# or camelCase in Java) for strings.
+All words must have their first letter capitalized
+without spaces.
+
+For instance:
+
+"hello case".camelCase() => HelloCase
+"camel case word".camelCase() => CamelCaseWord
+Don't forget to rate this kata! Thanks :)
+
+*/
+
+String.prototype.camelCase=function(){
+  //your code here
+  return this.split(' ').map((word) => word ? (word[0].toUpperCase() + word.slice(1)) : '').join('');
+}
+
+console.log("hello case".camelCase()); // HelloCase
